5ea56e31d9190fa854c3e2cdff96ccc1388f5e01
Scheltema <thomas.scheltema@hp.com>
2025-02-26 11:10:17 -0600

    Employee tests passing

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,8 +1,28 @@
 class Employee {
-
-
-
-    
+    #salary;
+    #isHired;
+    constructor(name, position, salary, isHired = true) {
+        this.name = name;
+        this.position = position;
+        this.#salary = salary;
+        this.#isHired = isHired;
+    }
+    getSalary() {
+        return this.#salary;
+    }
+    setSalary(newSalary) {
+        this.#salary = newSalary;
+    }
+    getStatus() {
+        return this.#isHired;
+    }
+    setStatus(command) {
+        if(command === "hire") {
+            this.#isHired = true;
+        } else if (command === "fire") {
+            this.#isHired = false;
+        }
+    }
 }
 
 module.exports = {
