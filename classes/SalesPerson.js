43805b90d730392b3667faeeb3d1f6e988af88bb
PhongT06 <Phongtt11@yahoo.com>
2025-02-26 14:52:57 -0600

    added salesperson and softwareengineer and updated employee class with static values

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,6 +1,18 @@
-class SalesPerson {
+const { Employee } = require("./Employee");
 
-
+class SalesPerson extends Employee{
+    #totalSales;
+    constructor(name, position, salary, clients, totalSales = 0){
+        super(name, position, salary); 
+        this.clients = clients || [];
+        this.#totalSales = totalSales;
+    }
+    getSalesNumber(){
+        return this.#totalSales;
+    }
+    makeSale(amount){
+        this.#totalSales += amount;
+    }
 
     
 }
